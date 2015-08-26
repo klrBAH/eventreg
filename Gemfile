23907773dd1cdb3646d7e33d660b28b73b200a72
Katie Rose <rose_katherine@bah.com>
2015-08-26 19:40:32 -0400

    Added Bootstrap, adding team lists to event. Monkey patch over "Event.scope" call in event_calendar

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'coffee-rails', '~> 4.1.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+# gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
@@ -62,8 +62,6 @@ gem 'geocoder', '~> 1.2.9'
 
 
 
-
-
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
@@ -73,5 +71,13 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+
+############ Non-standard
+	gem 'sextant', '~> 0.2.4'
+	gem 'better_errors', '~> 1.1.0'
+	gem "binding_of_caller", '~> 0.7.2'
+	gem 'meta_request', '~> 0.3.4'
+#############
 end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,22 @@
 class Event < ActiveRecord::Base
   has_event_calendar
+  has_many :event_team_users
+  has_many :users, :through => :event_team_users
+  has_many :teams
+
+
+include CoreExtensions::EventCalendar::ClassMethods
+
+      # Get the events overlapping the given start and end dates
+      # Monkey patching b/c "scoped" method no longer valid in Rails 4
+      def self.events_for_date_range(start_d, end_d, find_options = {})
+		conditions = [ "(? <= #{self.quoted_table_name}.#{self.end_at_field}) AND (#{self.quoted_table_name}.#{self.start_at_field}< ?)", start_d.to_time.utc, end_d.to_time.utc ]
+		order_string = "#{self.quoted_table_name}.#{self.start_at_field} ASC"
+		self.where(find_options).where(conditions).order(order_string)
+        # self.all.scoped(find_options).find(
+        #   :all,
+        #   :conditions => [ "(? <= #{self.quoted_table_name}.#{self.end_at_field}) AND (#{self.quoted_table_name}.#{self.start_at_field}< ?)", start_d.to_time.utc, end_d.to_time.utc ],
+        #   :order => "#{self.quoted_table_name}.#{self.start_at_field} ASC"
+        # )
+      end
 end
